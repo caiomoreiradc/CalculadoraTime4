b762c3db5a5f87aa09d1470ec442bde3590cd80d
caiomoreiradc <caiomoreiracarvalhoo@outlook.com>
2023-06-07 14:14:58 -0300

    Adicionado o Menu da Calculadora.

--- CalculadoraTime4.ConsoleApp/Program.cs
+++ CalculadoraTime4.ConsoleApp/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                string opcao = MostrarMenu();
+
+                if (opcao.Equals("s", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                //if (funçãodaopçãoinvalida(opcao))
+                    //cw "opção invalida"
+
+                //else if (opcao == "5")
+                    //TABUADA
+
+                //else if (opcao == "6")
+                    //VISUALIZAR OPERAÇÕES
+
+                //else
+                    //FunçãodeFazerOperação(opcao);
+            }
+
+
+        }
+        static string MostrarMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("==========================");
+            Console.WriteLine("    Calculadora Time 4");
+            Console.WriteLine("==========================");
+
+            Console.WriteLine("1-ADIÇÃO");
+            Console.WriteLine("2-SUBTRAÇÃO");
+            Console.WriteLine("3-MULTIPLICAÇÃO");
+            Console.WriteLine("4-DIVISÃO");
+            Console.WriteLine("5-TABUADA");
+            Console.WriteLine("6-HISTÓRICO DE OPERAÇÕES");
+            Console.WriteLine("S-SAIR");
+            Console.WriteLine("==========================");
+
+            string operacao = Console.ReadLine();
+
+            return operacao;
         }
     }
 }
