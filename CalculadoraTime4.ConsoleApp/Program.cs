5b57450424ff15f149156c5c3e0693d2fd6c9682
caiomoreiradc <caiomoreiracarvalhoo@outlook.com>
2023-06-07 13:54:49 -0300

    Add project files.

---
+++ CalculadoraTime4.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraTime4.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
