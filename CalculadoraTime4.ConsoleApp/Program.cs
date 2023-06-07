da34e4f2596cde5ba098e4ed336842a1b4338434
caiomoreiradc <caiomoreiracarvalhoo@outlook.com>
2023-06-07 15:53:04 -0300

    teste

--- CalculadoraTime4.ConsoleApp/Program.cs
+++ CalculadoraTime4.ConsoleApp/Program.cs
@@ -4,7 +4,131 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                string opcao = MostrarMenu();
+
+                if (opcao.Equals("s", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                if (opcao == "1")
+                    Adicao();
+
+                else if (opcao == "2")
+                    Subtrair();
+
+                else if (opcao == "3")
+                    Multiplicar();
+
+                else if (opcao == "4")
+                    Dividir();
+
+                //if (funçãodaopçãoinvalida(opcao))
+                //cw "opção invalida"
+
+                //else if (opcao == "5")
+                //TABUADA
+
+                //else if (opcao == "6")
+                //VISUALIZAR OPERAÇÕES
+
+                //else
+                //FunçãodeFazerOperação(opcao);
+            }
+
+            //teste
         }
+
+        static void Adicao()
+        {
+            Console.Clear();
+
+            Console.Write("Digite o primeiro número: ");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            double result = primeiroNumero + segundoNumero;
+
+            Console.WriteLine($"O resultado da soma é: {result}");
+
+            Console.ReadKey();
+        }
+
+        static void Subtrair()
+        {
+            Console.Clear();
+
+            Console.Write("Digite o primeiro número: ");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            double result = primeiroNumero - segundoNumero;
+
+            Console.WriteLine($"O resultado da subtração é: {result}");
+
+            Console.ReadKey();
+        }
+
+        static void Multiplicar()
+        {
+            Console.Clear();
+
+            Console.Write("Digite o primeiro número: ");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            double result = primeiroNumero * segundoNumero;
+
+            Console.WriteLine($"O resultado da multiplicação é: {result}");
+
+            Console.ReadKey();
+        }
+
+        static void Dividir()
+        {
+            Console.Clear();
+
+            Console.Write("Digite o primeiro número: ");
+            double primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+            double result = primeiroNumero / segundoNumero;
+
+            Console.WriteLine($"O resultado da multiplicação é: {result}");
+
+            Console.ReadKey();
+        }
+
+        static string MostrarMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("==========================");
+            Console.WriteLine("    Calculadora Time 4");
+            Console.WriteLine("==========================");
+
+            Console.WriteLine("1-ADIÇÃO");
+            Console.WriteLine("2-SUBTRAÇÃO");
+            Console.WriteLine("3-MULTIPLICAÇÃO");
+            Console.WriteLine("4-DIVISÃO");
+            Console.WriteLine("5-TABUADA");
+            Console.WriteLine("6-HISTÓRICO DE OPERAÇÕES");
+            Console.WriteLine("S-SAIR");
+            Console.WriteLine("==========================");
+
+            string operacao = Console.ReadLine();
+
+            return operacao;
+        }
+
+        //teste2
     }
 }
